dfcab4a514d8a2fff66749c236d683fe4f8ef563
Eleonora27-mon <monterisieleonora@gmail.com>
2024-12-10 11:42:19 +0100

    Passo 2: completato!

--- src/App.java
+++ src/App.java
@@ -9,16 +9,33 @@ public class App {
 
         String pescaCarta = "no";
         double punteggioUtente = 0;
-
+        int seme;
+        
+        String nomeSeme = "";
+    
+         
+        
         do {
             int cartaUtente = r.nextInt(10) + 1;
+            seme=r.nextInt(4)+1;
+            switch (seme) {
+                case 1: nomeSeme = "denari";
+                    break;
+                case 2: nomeSeme = "bastoni";
+                break;
+                case 3: nomeSeme = "spade";
+                break;
+                case 4: nomeSeme = "coppe";
+                break;
+             }
+                
             // punteggioUtente = punteggioUtente + cartaUtente;
             if (cartaUtente >= 8) {
                 punteggioUtente = punteggioUtente + 0.5;
             } else {
                 punteggioUtente = punteggioUtente + cartaUtente;
             }
-            System.out.println("\nHai pescato la carta " + cartaUtente);
+            System.out.println("\nHai pescato la carta " + cartaUtente + " di " + nomeSeme);
             System.out.println("Punteggio corrente " + punteggioUtente);
 
             pescaCarta = "no";
@@ -43,4 +60,5 @@ public class App {
 
 
     }
-}
+    }
+
